2a2ded068f608b35f5ce204c7915353a9e33813f
Soszka <bsocha1998@gmail.com>
2023-05-31 20:37:55 +0200

    monuments data added to data.js

---
+++ js/data.js
@@ -0,0 +1,27 @@
+const dataSource = {
+
+  monuments: {
+    statue: {
+      name: "Pomnik",
+      description: "Structure or sculpture, usually made of durable materials.",
+      img: "https://i.postimg.cc/KjfC3pJ8/Monuments-Foto-1.jpg"
+    },
+    statue: {
+      name: "Katedra",
+      description: "Higher Church, which is usually the seat of a bishop or archbishop.",
+      img: "https://i.postimg.cc/gkMfY5xM/Monuments-Foto-3.jpg"
+    },
+    statue: {
+      name: "Zamek",
+      description: "Defensive structure, which had defensive and residential functions.",
+      img: "https://i.postimg.cc/v8tJKDbw/Monuments-Foto-4.jpg"
+    },
+    statue: {
+      name: "Ratusz",
+      description: "The building where it is located municipal authorities.",
+      img: "https://i.postimg.cc/15Rk1P3n/Monuments-Foto-2.jpg"
+    },
+  }
+};
+
+export default dataSource;
