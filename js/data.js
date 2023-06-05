0c369c0d301fb385418152d76303d5b63b6fcc49
Soszka <bsocha1998@gmail.com>
2023-06-05 21:07:27 +0200

    'entertain' functions added

--- js/data.js
+++ js/data.js
@@ -47,23 +47,21 @@ const dataSource = {
       id: 'party',
       icon: "fa-solid fa-champagne-glasses",
       image: 'https://i.postimg.cc/Qts2RT4Q/Entertain-Foto-1.jpg',
-      description: `Experience Rzeszów's vibrant nightlife! Dance the night away in clubs and bars, with pulsating beats and lively atmospheres.
-       From trendy clubs to cozy pubs with live music, Rzeszów offers a diverse party scene that caters to all tastes.
+      description: `Experience Rzeszów's vibrant nightlife! Dance the night away in clubs and bars, with pulsating beats and lively atmospheres. From trendy clubs to cozy pubs with live music.
        Enjoy themed parties and unforgettable nights in the city's energetic nightlife.`
     },
     {
       id: 'boating',
       icon: "fa-solid fa-anchor",
       image: 'https://i.postimg.cc/2ySP5ff3/Entertain-Foto-3.jpg',
-      description: `Explore Rzeszów's serene lakes and rivers. Rent a boat or kayak and immerse yourself in nature's beauty.
-       Whether you seek tranquility on a peaceful lake or adventure along a winding river,
-       Rzeszów provides a refreshing boating experience for all levels. Discover the wonders of Rzeszów's waterscapes and connect with nature.`
+      description: `Explore Rzeszów's serene lakes and rivers. Whether you seek tranquility on a peaceful lake or adventure along a winding river,
+      Discover the wonders of Rzeszów's waterscapes and connect with nature.`
     },
     {
       id: 'skatepark',
       icon: "fa-solid fa-person-skating",
       image: 'https://i.postimg.cc/6qbF219c/Entertain-Foto-5.jpg',
-      description: `Rzeszów is a haven for skateboarders and extreme sports enthusiasts. Test your skills at modern skateparks designed for all levels.
+      description: `Rzeszów is a haven for skateboarders and extreme sports enthusiasts.
        Ride ramps, rails, and obstacles in an inclusive and thrilling environment. 
        Whether you're a beginner or a pro, Rzeszów's skateparks are perfect for adrenaline-pumping fun.`
     },
@@ -71,15 +69,15 @@ const dataSource = {
       id: 'bowling',
       icon: "fa-solid fa-bowling-bal",
       image: 'https://i.postimg.cc/DZg9FwHt/Entertain-Foto-4.jpg',
-      description: `Looking for indoor fun? Rzeszów has you covered with its fantastic bowling alleys. Gather your friends or family and enjoy a friendly game.
-       Rzeszów's bowling centers offer multiple lanes, comfortable seating, and a lively atmosphere. Whether you're a seasoned pro aiming for a strike or a beginner just having fun,
+      description: `Looking for indoor fun? Rzeszów has you covered with its fantastic bowling alleys.
+       Rzeszów's bowling centers offer multiple lanes, comfortable seating, and a lively atmosphere.
        Roll your way to a great time, indulge in delicious snacks, and enjoy the social ambiance.`
     },
     {
       id: 'cinema',
       icon: "fa-solid fa-film",
       image: 'https://i.postimg.cc/PrNgsh4R/Entertain-Foto-6.jpg',
-      description: `Escape into the world of film at Rzeszów's cinemas. Catch the latest blockbusters, art-house films, and international releases.
+      description: `Escape into the world of film at Rzeszów's cinemas.
        With comfortable seating and top-notch screens, Rzeszów's modern multiplexes offer an immersive movie experience.
        Discover a variety of genres and let the magic of cinema transport you in Rzeszów's cozy theaters.`
     }
--- js/script.js
+++ js/script.js
@@ -4,20 +4,24 @@ import { dataSource } from './data.js';
 // Settings
 
 const select = {
-  optMonumentsListSelector: '.monuments__list .row',
-  optPreviewItemsListSelector: '.preview__carousel .carousel-inner'
+  monumentsListSelector: '.monuments__list .row',
+  previewItemsListSelector: '.preview__carousel .carousel-inner',
+  entertainLinksListSelector: '.entertain__list ul',
+  entertainItemsListSelector: '.entertain__items'
 }
 
 const template = {
   monumentsItem: Handlebars.compile(document.querySelector('#template-monument-item').innerHTML),
-  previewCarouselItem: Handlebars.compile(document.querySelector('#template-preview-carousel-item').innerHTML)
+  previewCarouselItem: Handlebars.compile(document.querySelector('#template-preview-carousel-item').innerHTML),
+  entertainLink: Handlebars.compile(document.querySelector('#template-entertain-link').innerHTML),
+  entertainItem: Handlebars.compile(document.querySelector('#template-entertain-item').innerHTML),
 };
 
 
 // Preview
 
 function generatePreviewCarouselItems() {
-  const carouselItemsList = document.querySelector(select.optPreviewItemsListSelector);
+  const carouselItemsList = document.querySelector(select.previewItemsListSelector);
   const carouselItemsHTML = template.previewCarouselItem(dataSource);
   carouselItemsList.innerHTML = carouselItemsHTML;
   const firstCarouselItem = document.querySelector('.carousel-item');
@@ -29,7 +33,7 @@ generatePreviewCarouselItems();
 // Monuments
 
 function generateMonumentsItems() {
-  const monumentsList = document.querySelector(select.optMonumentsListSelector);
+  const monumentsList = document.querySelector(select.monumentsListSelector)
   const monumentsHTML = template.monumentsItem(dataSource);
   monumentsList.innerHTML = monumentsHTML;
 }
@@ -37,4 +41,47 @@ generateMonumentsItems();
 
 
 // Entertain
+
+function generateEntertainLinks() {
+  const entertainLinksList = document.querySelector(select.entertainLinksListSelector);
+  const entertainLinkHTML = template.entertainLink(dataSource);
+  entertainLinksList.innerHTML = entertainLinkHTML;
+  const firstEntertainLink = document.querySelector('.entertain__list a');
+  firstEntertainLink.classList.add('active');
+  console.log(firstEntertainLink);
+}
+generateEntertainLinks()
+
+
+function generateEntertainItems() {
+  const entertainItemsList = document.querySelector(select.entertainItemsListSelector);
+  const entertainItemHTML = template.entertainItem(dataSource);
+  entertainItemsList.innerHTML = entertainItemHTML;
+  const firstEntertainItem = document.querySelector('.entertain__item ');
+  firstEntertainItem.classList.add('active');
+}
+generateEntertainItems()
+
+
+function entertainClickHandler(event) {
+  event.preventDefault();
+  const activeLink = document.querySelector('.entertain__list a.active');
+  activeLink.classList.remove('active');
+  const clickedLink = this;
+  clickedLink.classList.add('active');
+  const entertainItemId = clickedLink.getAttribute('href');
+  const activeEntertainItem = document.querySelector('.entertain__items .active');
+  activeEntertainItem.classList.remove('active');
+  const targetEntertainItem = document.querySelector(entertainItemId);
+  targetEntertainItem.classList.add('active');
+}
+
+function addClickListenersToEntertainLinks() {
+  let links = document.querySelectorAll('.entertain__list a');
+  for (let link of links) {
+    link.addEventListener('click', entertainClickHandler);
+  }
+}
+addClickListenersToEntertainLinks();
+
 
