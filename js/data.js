a8c3f312caa3b53dec1c487f6d8008fc06e0da37
Soszka <bsocha1998@gmail.com>
2023-12-01 12:09:47 +0100

    Page updated

--- js/data.js
+++ js/data.js
@@ -1,85 +1,88 @@
 const dataSource = {
 
   previewCarouselItems : [
-    { name: 'Rzeszowskei Wały', image: 'https://i.postimg.cc/8zgQSByr/Preview-Foto-2.jpg' },
-    { name: 'Bulwary', image: 'https://i.postimg.cc/tg9K1RGq/Preview-Foto-3.jpg' },
-    { name: 'Lisia Góra', image: 'https://i.postimg.cc/RZ1rV68p/Preview-Foto-4.jpg' },
-    { name: 'Galeria Rzeszów', image: 'https://i.postimg.cc/N0bWRhQ1/Preview-Foto-5.jpg' }
+    { name: 'Main Center', image: 'https://i.postimg.cc/8zgQSByr/Preview-Foto-2.jpg' },
+    { name: 'City River', image: 'https://i.postimg.cc/tg9K1RGq/Preview-Foto-3.jpg' },
+    { name: 'Old Town', image: 'https://i.postimg.cc/RZ1rV68p/Preview-Foto-4.jpg' },
+    { name: 'Huge Bridge', image: 'https://i.postimg.cc/N0bWRhQ1/Preview-Foto-5.jpg' }
   ],
 
   monumentsItems: [
     {
-      name: 'Pomnik',
+      name: 'Statue',
       description: 'Structure or sculpture, usually made of durable materials.',
       img: 'https://i.postimg.cc/KjfC3pJ8/Monuments-Foto-1.jpg',
       link: 'https://github.com/Soszka/SCSS-cityLayout'
     },
     {
-      name: 'Katedra',
+      name: 'Cathedral',
       description: 'Higher Church, which is usually the seat of a bishop or archbishop.',
       img: 'https://i.postimg.cc/gkMfY5xM/Monuments-Foto-3.jpg',
-      link: 'https://github.com/Soszka/SCSS-cityLayout'
+      link: 'https://github.com/Soszka/SCSS-cityLayout',
+      delay: '250'
     },
     {
-      name: 'Zamek',
-      description: 'Defensive structure, which had defensive and residential functions.',
+      name: 'Castle',
+      description: 'Defensive structure, which had residential functions.',
       img: 'https://i.postimg.cc/v8tJKDbw/Monuments-Foto-4.jpg',
-      link: 'https://github.com/Soszka/SCSS-cityLayout'
+      link: 'https://github.com/Soszka/SCSS-cityLayout',
+      delay: '500',
     },
     {
-      name: 'Ratusz',
+      name: 'Town Hall',
       description: 'The building where it is located municipal authorities.',
       img: 'https://i.postimg.cc/15Rk1P3n/Monuments-Foto-2.jpg',
-      link: 'https://github.com/Soszka/SCSS-cityLayout'
+      link: 'https://github.com/Soszka/SCSS-cityLayout',
+      delay: '750'
     }
   ],
 
   entertainItems: [
     {
       id: 'swimming',
-      icon: "fa-solid fa-person-swimming",
+      icon: 'fa-solid fa-person-swimming',
       image: 'https://i.postimg.cc/jS2Z2G0Q/Entertain-Foto-2.jpg',
-      description: `If you like swimming, you will surely find something for yourself in Rzeszów.
+      description: `If you like swimming, you will surely find something for yourself in the City.
         Many indoor pools as well as those open in the summer are at your full disposal.
         There are also clubs and swimming schools that offer training for both beginners and more advanced swimmers.`
     },
     {
       id: 'party',
-      icon: "fa-solid fa-champagne-glasses",
+      icon: 'fa-solid fa-champagne-glasses',
       image: 'https://i.postimg.cc/Qts2RT4Q/Entertain-Foto-1.jpg',
-      description: `Experience Rzeszów's vibrant nightlife! Dance the night away in clubs and bars, with pulsating beats and lively atmospheres. From trendy clubs to cozy pubs with live music.
+      description: `Experience City's vibrant nightlife! Dance the night away in clubs and bars, with pulsating beats and lively atmospheres. From trendy clubs to cozy pubs with live music.
        Enjoy themed parties and unforgettable nights in the city's energetic nightlife.`
     },
     {
       id: 'boating',
-      icon: "fa-solid fa-anchor",
+      icon: 'fa-solid fa-anchor',
       image: 'https://i.postimg.cc/2ySP5ff3/Entertain-Foto-3.jpg',
-      description: `Explore Rzeszów's serene lakes and rivers. Whether you seek tranquility on a peaceful lake or adventure along a winding river,
-      Discover the wonders of Rzeszów's waterscapes and connect with nature.`
+      description: `Explore City's serene lakes and rivers. Whether you seek tranquility on a peaceful lake or adventure along a winding river,
+      Discover the wonders of City's waterscapes and connect with nature.`
     },
     {
       id: 'skatepark',
-      icon: "fa-solid fa-person-skating",
+      icon: 'fa-solid fa-person-skating',
       image: 'https://i.postimg.cc/6qbF219c/Entertain-Foto-5.jpg',
-      description: `Rzeszów is a haven for skateboarders and extreme sports enthusiasts.
+      description: `The City is a haven for skateboarders and extreme sports enthusiasts.
        Ride ramps, rails, and obstacles in an inclusive and thrilling environment. 
-       Whether you're a beginner or a pro, Rzeszów's skateparks are perfect for adrenaline-pumping fun.`
+       Whether you're a beginner or a pro, City's skateparks are perfect for adrenaline-pumping fun.`
     },
     {
       id: 'bowling',
-      icon: "fa-solid fa-bowling-ball",
+      icon: 'fa-solid fa-bowling-ball',
       image: 'https://i.postimg.cc/DZg9FwHt/Entertain-Foto-4.jpg',
-      description: `Looking for indoor fun? Rzeszów has you covered with its fantastic bowling alleys.
-       Rzeszów's bowling centers offer multiple lanes, comfortable seating, and a lively atmosphere.
+      description: `Looking for indoor fun? The City has you covered with its fantastic bowling alleys.
+       City's bowling centers offer multiple lanes, comfortable seating, and a lively atmosphere.
        Roll your way to a great time, indulge in delicious snacks, and enjoy the social ambiance.`
     },
     {
       id: 'cinema',
-      icon: "fa-solid fa-film",
+      icon: 'fa-solid fa-film',
       image: 'https://i.postimg.cc/PrNgsh4R/Entertain-Foto-6.jpg',
-      description: `Escape into the world of film at Rzeszów's cinemas.
-       With comfortable seating and top-notch screens, Rzeszów's modern multiplexes offer an immersive movie experience.
-       Discover a variety of genres and let the magic of cinema transport you in Rzeszów's cozy theaters.`
+      description: `Escape into the world of film at City's cinemas.
+       With comfortable seating and top-notch screens, City's modern multiplexes offer an immersive movie experience.
+       Discover a variety of genres and let the magic of cinema transport you in City's cozy theaters.`
     }
   ],
 
@@ -98,7 +101,7 @@ const dataSource = {
   educationItems: [
     {
       id: 'flush-collapseOne',
-      name: 'Politechnika mieszcząca się w Rzeszowie',
+      name: 'University of Technology located in City',
       image: 'https://i.postimg.cc/8cw2TT4s/Education-Foto-2.jpg',
       description: `A technical university that offers undergraduate, graduate,
       and doctoral studies in various fields of science, such as mechanical engineering,
@@ -109,7 +112,7 @@ const dataSource = {
     },
     {
       id: 'flush-collapseTwo',
-      name: 'Uniwersytet mieszczący się w Rzeszowie',
+      name: 'University located in City',
       image: 'https://i.postimg.cc/mrJ2GyDK/Education-Foto-1.jpg',
       description: `A comprehensive university that offers studies in various fields,
       such as social sciences, humanities, natural sciences, or medicine.
@@ -119,7 +122,7 @@ const dataSource = {
     },
     {
       id: 'flush-collapseThree',
-      name: 'Szkoła policealna mieszcząca się w Rzeszowie',
+      name: 'Post-secondary school located in the City,',
       image: 'https://i.postimg.cc/tgsR2yX6/Education-Foto-3.jpg',
       description: `A vocational school is a type of secondary school that offers education programs at the level of technical school or vocational courses.
       It often emphasizes practical skills, preparing students for specific professions.
